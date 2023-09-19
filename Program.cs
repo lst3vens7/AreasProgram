f06b1ff1e5fee9c4ab0e8fa33e7fb750881d0d0f
lst3vens7 <145001217+lst3vens7@users.noreply.github.com>
2023-09-19 10:07:01 +0100

    Added main program

--- Program.cs
+++ Program.cs
@@ -10,6 +10,71 @@ namespace AreasProgram
     {
         static void Main(string[] args)
         {
+            double area;
+            double perimeter;
+
+            Console.WriteLine("What would you like to find the area and perimeter for?");
+            Console.WriteLine("(c for circle, r for rectangle, t for triangle) ");
+
+            string shape = Console.ReadLine();
+
+            if (shape == "c")
+            {
+                Console.WriteLine("Please enter the radius of the circle: ");
+                double r = Convert.ToDouble(Console.ReadLine());
+                
+                Circle circle = new Circle();
+
+                area = circle.Area(r);
+                perimeter = circle.Perimeter(r);
+
+                Console.WriteLine("The area of the circle is: {0}", area);
+                Console.WriteLine("The perimeter of the circle is: {0}", perimeter);
+            }
+            else if (shape == "r")
+            {
+                Console.WriteLine("Please enter the height of the rectangle: ");
+                double h = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please enter the width of the rectangle: ");
+                double w = Convert.ToDouble(Console.ReadLine());
+
+                Rectangle rectangle = new Rectangle();
+
+                area = rectangle.Area(h, w);
+                perimeter = rectangle.Perimeter(h, w);
+
+                Console.WriteLine("The area of the rectangle is: {0}", area);
+                Console.WriteLine("The perimeter of the rectangle is: {0}", perimeter);
+            }
+            else if (shape == "t") 
+            {
+                Console.WriteLine("Please enter the length of side a: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please enter the length of side b: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Please enter the length of side c: ");
+                double c = Convert.ToDouble(Console.ReadLine());
+
+                if (a <= b+c && b <= a+c && c <= a+b)
+                {
+                    Triangle triangle = new Triangle();
+
+                    area = triangle.Area(a,b,c);
+                    perimeter = triangle.Perimeter(a, b, c);
+
+                    Console.WriteLine("The area of the triangle is: {0}", area);
+                    Console.WriteLine("The perimeter of the triangle is: {0}", perimeter);
+                }
+                else
+                {
+                    Console.WriteLine("The inputted triangle is invalid!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+            Console.ReadLine();
         }
     }
 }
